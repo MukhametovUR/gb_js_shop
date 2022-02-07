49115f9cfa5861e265dbfe7d97798913ffaa4ab8
MukhametovUR <ural-lan4x4@yandex.ru>
2022-02-07 16:24:29 +0300

    Добавлена сумма всех товаров, добавлены методы для работы с товарами

--- project/js/main.js
+++ project/js/main.js
@@ -1,40 +1,64 @@
-const products = [
-    {   id: 1, 
-        title: 'Notebook', 
-        price: 2000,
-        img: './img/notebook.jpg'  
-},
-    {   id: 2, 
-        title: 'Mouse', 
-        price: 20,
-        img: './img/mouse.jpg'
-    },
-    {
-        id: 3, title:'Keyboard', 
-        price: 200,
-        img: './img/keyboard.jpg'},
-    {   id: 4, 
-        title: 'Gamepad', 
-        price: 50,
-        img: './img/gamepad.jpg'},
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (title, price, img) => {
-    return `<div class="product-item">
+class ProductList{
+    constructor(container='.products'){
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
+        this.render();//вывод товаров на страницу
+        this.sumItems();
+    }
+    _fetchProducts(){
+        this.goods = [
+        {id: 1, title: 'Notebook',price: 2000, image: './img/notebook.jpg'},
+        {id: 2, title: 'Mouse',price: 20, image: './img/mouse.jpg'},
+        {id: 3, title: 'Keyboard',price: 200, image:'./img/keyboard.jpg'},
+        {id: 4, title: 'Gamepad',price: 50, image: './img/gamepad.jpg'},
+        ];
+    }
+
+    sumItems(){
+        let sum = this.goods.map(item =>item.price)
+                            .reduce((a,b) => a+b);        
+        console.log(` суммарную стоимость всех товаров ${sum}`)
+      }
+
+    addItem(){
+
+    }
+
+    deleteItem(){
+
+    }
+
+    updateItem(){
+
+    }
+
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+             const item = new ProductItem(product);
+             block.insertAdjacentHTML("beforeend",item.render());
+        }
+    }
+}
+class ProductItem{
+    constructor(product){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.image = product.image;
+    }
+    render(){
+            return `<div class="product-item">
             <div class="item-img">
-                <img src=${img} alt="img">
+                <img src=${this.image} alt="img">
             </div>
-                <h3>${title}</h3>
-                <p>${price}</p>
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title,item.price,item.img));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join('');
-};
+    }
+}
 
-renderPage(products);
 
+let list = new ProductList();
