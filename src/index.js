9d1d3a1704eefe70f6a1860b182dfbdb20aab0ee
reglapareto <ural.muhkhametov@gmail.com>
2022-02-11 01:24:02 +0300

    Созданы классы в main.js для расширения проекта

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-
-import ProductItem from "./productItem";
-import ProductList from "./productList";
-import list from "./productList";
-import listBaskets from './basket'
-
-import './scss/main.scss'
-
-// import list from './ProductItem';
-
-
-
-// const list = new ProductList();
-// list._getProducts(() => {
-//     list.render();
-// });
+import list from "./main";
 
 
